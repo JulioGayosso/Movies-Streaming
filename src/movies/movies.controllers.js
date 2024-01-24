7fd72b55a5f8e3068b9f682e62142f4d70e99dcf
juliogayosso <j_chalo@hotmail.com>
2024-01-24 12:19:37 -0600

    feature: add searchs

--- src/movies/movies.controllers.js
+++ src/movies/movies.controllers.js
@@ -1,31 +1,32 @@
-const Movies = require('../models/movies.models')
-const MovieGenres = require('../models/movie_genres.models')
-const uuid = require('uuid')
-const Genres = require('../models/genres.models')
-const {Op} = require('sequelize')
+const Movies = require("../models/movies.models");
+const MovieGenres = require("../models/movie_genres.models");
+const uuid = require("uuid");
+const Genres = require("../models/genres.models");
+const { Op } = require("sequelize");
 
-const FindAllmovies = async(limit,offset,search) =>{
+const FindAllmovies = async (limit, offset, search) => {
   // limit -> cuantos quiero mostrar
- //  offset -> donde empiezo a mostrar
-  
-    const queryOptions = {
-       limit: limit,
-       offset: offset ,
-       where:{}
-    }
+  //  offset -> donde empiezo a mostrar
 
-    if(search){
-      queryOptions.where = {
-        title:{
-          
-          [Op.iLike]: `%${search}%`
-        }
-      }
-    }
- 
-    const data = await Movies.findAndCountAll(queryOptions)
-    return data
-}
+  const queryOptions = {
+    limit: limit,
+    offset: offset,
+    where: {},
+  };
+
+  if (search) {
+    queryOptions.where = {
+      title: {
+        [Op.iLike]: `%${search}%`,
+        //Like -> Case sensitive -> diferencia entre mayusculas y minusculas
+        //iLike  -> Not case sensitive -> No genera distincion entre mayusculas y minusculas
+      },
+    };
+  }
+
+  const data = await Movies.findAndCountAll(queryOptions);
+  return data;
+};
 
 // controlador
 
@@ -37,44 +38,41 @@ const createMovie = async (movieObj) => {
     releaseYear: movieObj.releaseYear,
     director: movieObj.director,
     duration: movieObj.duration,
-    traillerUrl: movieObj.traillerUrl,  // Asegúrate de que la ortografía sea la misma que en el modelo
+    traillerUrl: movieObj.traillerUrl, // Asegúrate de que la ortografía sea la misma que en el modelo
     coverUrl: movieObj.coverUrl,
     movieUrl: movieObj.movieUrl,
-    clasification: movieObj.clasification,  // También aquí, asegúrate de que la ortografía sea la misma que en el modelo
-    rating: movieObj.rating
+    clasification: movieObj.clasification, // También aquí, asegúrate de que la ortografía sea la misma que en el modelo
+    rating: movieObj.rating,
   };
 
   const data = await Movies.create(newMovie);
   return data;
 };
 
-const addGenreToMovie = async (dataObj) =>{
-  const data = await  MovieGenres.create({
-    id:uuid.v4(),
-    movieId:dataObj.movieObj,
-    genreId:genreId.movieObj
-  })
-  return data
-}
+const addGenreToMovie = async (dataObj) => {
+  const data = await MovieGenres.create({
+    id: uuid.v4(),
+    movieId: dataObj.movieObj,
+    genreId: genreId.movieObj,
+  });
+  return data;
+};
 
-const findAllMoviesByGenre = async(genreId) => {
+const findAllMoviesByGenre = async (genreId) => {
   const data = await Movies.findAll({
-     include:{
-       model:Genres,
-       where:{
-         id:genreId
-       }
-     }
-  })
-  return data
-}
-
+    include: {
+      model: Genres,
+      where: {
+        id: genreId,
+      },
+    },
+  });
+  return data;
+};
 
 module.exports = {
   FindAllmovies,
   createMovie,
   addGenreToMovie,
-  findAllMoviesByGenre
+  findAllMoviesByGenre,
 };
-
-
